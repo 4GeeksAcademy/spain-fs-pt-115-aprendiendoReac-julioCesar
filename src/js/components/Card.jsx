ae80608d6f2a486529c09a522d98749d0fc5790b
Julio Cesar <juliowork.1309@gmail.com>
2025-07-07 23:38:31 +0000

    PokerStart

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -2,7 +2,7 @@ export const Card = (props) => {
     const {name, imagen, description, button, buttonURL} = props
     return (
         <div className="col-md-3">
-            <div className="card" style={{width: "18rem;"}}>
+            <div className="card" style={{width: "18rem"}}>
                 <img src={imagen} className="card-img-top" alt="ejemplo" style={{ height: "200px", objectFit: "cover" }}/>
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer className="bg-dark text-center mt-5 py-3">
+            <p className="text-white mb-0">
+                Copyright © 2001–2025, Rational Intellectual Holdings Limited. Todos los derechos reservados.
+            </p>
+        </footer>
+    );
+};
